37852648a0473610fa7f5938a3fe37f3d71fefc2
Ralf Yang <goody80762@gmail.com>
2015-08-03 17:03:32 +0900

    update processor

---
+++ repo_update.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#BaseRoot=`cat /usr/bin/zinst |grep "ZinstBaseRoot=" | awk -F'=' '{print $2}' | sed -s 's/"//g'`
+#WorkDIR=$BaseRoot/dist
+WorkDIR=$PWD
+
+ls -ali  |grep "\.zinst" | awk '{print $9}' > pkglist
+mkdir -p $WorkDIR/checker 2> /dev/null
+ListUpArry=( `ls $WorkDIR |grep ".zinst"` )
+Count=0
+
+	while [ $Count -lt ${#ListUpArry[@]} ] 
+	do
+		cd $WorkDIR/checker
+		OrgName=`echo "${ListUpArry[$Count]}" | awk -F '-' '{print $1}'`
+		tar zxvfp $WorkDIR/${ListUpArry[$Count]} $OrgName.zicf -C $WorkDIR/checker
+		mv $WorkDIR/checker/$OrgName.zicf $WorkDIR/checker/${ListUpArry[$Count]}.zicf
+	let Count=$Count+1
+	done
+
+sudo chmod 775 -R $WorkDIR/*
+sudo chgrp wheel -R $WorkDIR/*
+gitpush
+gitpush-branch
